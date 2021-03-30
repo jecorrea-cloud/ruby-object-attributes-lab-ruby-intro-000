d9cd5602789147f48ea609f7f0e5726c3a690918
Joaquin Correa <jecorrea@protonmail.com>
2021-03-30 22:46:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,7 +9,7 @@ class Person
     @name
   end
 
-  def name = (new_name)
+  def name= (new_name)
     @name = new_name
   end
 
@@ -17,7 +17,7 @@ class Person
     @job
   end
 
-  def job = (new_job)
+  def job= (new_job)
     @job = new_job
   end
 
